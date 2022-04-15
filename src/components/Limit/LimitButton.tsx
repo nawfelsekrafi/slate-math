4b3cfde4947bc9936ac1dbc50f997d22dbf8034e
leiawang89 <leiawang89@gmail.com>
2022-04-15 14:09:12 -0700

    add tool tip for each type of limit

--- src/components/Limit/LimitButton.tsx
+++ src/components/Limit/LimitButton.tsx
@@ -1,15 +1,25 @@
-interface LimitButtonProps {
+import Tippy from '@tippyjs/react'
+import { getLimitButtonStyles } from './getLimitButtonStyles'
+
+type LimitButtonProps = {
   name: string
+  limitName: any
   image: any
   onClick: () => void
 }
 
-export const LimitButton = ({ image, onClick }: LimitButtonProps) => {
-  return (
-    <div>
-      <button onClick={onClick}>
+export const LimitButton = ({
+  image,
+  limitName,
+  onClick,
+}: LimitButtonProps) => {
+  const styles = getLimitButtonStyles()
+  const content = (
+    <div className="limitButtons">
+      <button className="accentButton" onClick={onClick} css={styles.root.css}>
         <img src={image} />
       </button>
     </div>
   )
+  return limitName ? <Tippy content={limitName}>{content}</Tippy> : content
 }
--- src/components/Limit/LimitPicker.tsx
+++ src/components/Limit/LimitPicker.tsx
@@ -7,27 +7,28 @@ import { LimitButton } from './LimitButton'
 import { LimitType } from './LimitType'
 import { getEmptyLimNode, getLimToInfinityNode } from './getEmptyLimNode'
 
-function initializeEquationMap(): {
-  nameToClickFuncMap: any
-  nameToImgMap: any
-} {
+const initializeEquationMap = () => {
   const editor = getPlateEditorRef()!
   const nameToClickFuncMap = new Map<string, any>()
-
   nameToClickFuncMap.set('1x1', () => {
     insertMathNode(getEmptyLimNode, editor)
     selectFirstBox(editor)
   })
 
-    nameToClickFuncMap.set('1x2', () => {
-      insertMathNode(getLimToInfinityNode, editor)
-      selectFirstBox(editor)
-    })
+  nameToClickFuncMap.set('1x2', () => {
+    insertMathNode(getLimToInfinityNode, editor)
+    selectFirstBox(editor)
+  })
 
   const nameToImgMap = new Map<string, any>()
   nameToImgMap.set('1x1', Limit)
   nameToImgMap.set('1x2', LimitToInfinity)
-  return { nameToClickFuncMap, nameToImgMap }
+
+  const nameToLimitNameMap = new Map<string, string>()
+  nameToLimitNameMap.set('1x1', 'Limit')
+  nameToLimitNameMap.set('1x2', 'Limit To Infinity')
+
+  return { nameToClickFuncMap, nameToImgMap, nameToLimitNameMap }
 }
 
 type LimitPickerProps = {
@@ -36,12 +37,14 @@ type LimitPickerProps = {
 }
 
 export const LimitPicker = ({ limits }: LimitPickerProps) => {
-  const { nameToClickFuncMap, nameToImgMap } = initializeEquationMap()
+  const { nameToClickFuncMap, nameToImgMap, nameToLimitNameMap } =
+    initializeEquationMap()
   return (
     <div className="limit-container" id="limitContainer">
       {limits.map(({ name }) => (
         <LimitButton
           name={name}
+          limitName={nameToLimitNameMap.get(name)}
           image={nameToImgMap.get(name)}
           onClick={nameToClickFuncMap.get(name)}
         />
